c85fcfb6a06ba1024367a8b22df4611f917e4440
João Paulo <joaopaulo761@gmail.com>
2018-06-14 13:09:09 -0300

    Mudança de modo de jogo

---
+++ Assets/Scripts/Game/GameModeChanger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameModeChanger : MonoBehaviour {
+    public GameManager.GameMode gameMode = GameManager.GameMode.BOSS;
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("Player")) {
+            GameManager.Get().gameMode = gameMode;
+        }
+    }
+}
